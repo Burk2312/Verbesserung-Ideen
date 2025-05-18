232d0271999c3cde89806c821ebcac48473034cf
Burk2312 <bg@gieloff.de>
2025-05-18 19:06:24 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Willkommen zu deinem Verbesserungsvorschlagsprogramm</h1>
+            <p>Besuche die <a href="/vorschlag">Vorschlagsseite</a>, um einen Vorschlag einzureichen.</p>
+        </div>
+    );
+}
