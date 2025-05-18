b8c2b34267114258ce6822744ea1b6fa97de13dc
Burk2312 <bg@gieloff.de>
2025-05-18 19:38:49 +0200

    Create vorschlag.js

---
+++ pages/vorschlag.js
@@ -0,0 +1,10 @@
+// pages/vorschlag.js
+import Vorschlagsformular from '../components/Vorschlagsformular';
+
+export default function VorschlagPage() {
+    return (
+        <div className="container mx-auto py-10 bg-gray-100 min-h-screen">
+            <Vorschlagsformular />
+        </div>
+    );
+}
