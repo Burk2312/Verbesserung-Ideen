d015210bcb6e057abe4164d8ff8d10811e0bbd33
Burk2312 <bg@gieloff.de>
2025-05-19 15:36:46 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        secondary: '#1D4ED8',
+      },
+    },
   },
   plugins: [],
 }
